4311ba8c207c67793e94e4fd9e650da3ae7ca578
Gr8th1ngy <anhhuyapmops@gmail.com>
2019-03-05 21:26:22 -0500

    Add asteroids

---
+++ Space shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    float maxZValue;
+    float maxXValue;
+
+    private void Start()
+    {
+        Vector3 screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f));
+        maxZValue = screenBounds.z * 1.2f;
+        maxXValue = screenBounds.x * 0.9f;
+
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+
+                Vector3 spawnPosition = new Vector3(Random.Range(-maxXValue, maxXValue), 0f, maxZValue);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+}
